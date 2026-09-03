651ef6134537f1ab959cf4c6667daee710750df7
Greedzz-cmd <rakib123kib.ra@gmail.com>
2026-09-04 02:57:01 +0600

    Add /tickets route with isAdvertised filter and CORS config

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors")
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 dotenv.config();
 
 const app = express();
+app.use(express.json())
+app.use(cors({origin:process.env.CLIENT_URL}))
 
 const port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
@@ -25,13 +28,19 @@ async function run() {
 
         console.log("Successfully connected to MongoDB!");
 
-        app.get("/", async (req, res) => {
+        app.get("/tickets", async (req, res) => {
             try {
-                const db = client.db("ticket-bari");
+                const db = client.db("routely");
                 const collection = db.collection("tickets");
-
-                const tickets = await collection.find().toArray();
-
+                
+                const {isAdvertised} = req.query;
+                
+                const query = {}
+                if(isAdvertised !== undefined){
+                    query.isAdvertised = isAdvertised==="true";
+                }
+                
+                const tickets = await collection.find(query).toArray();
                 res.send(tickets);
             } catch (error) {
                 console.error(error);
